1fe77f0c0d1cd0d224385b9b3c6c5aacd07b8848
Alixdo <alixdodu@gmail.com>
2015-01-08 11:17:05 +0000

    Second script file to keep map script appart

--- script.js
+++ script.js
@@ -1,102 +1,14 @@
-d3.select(window).on("resize", throttle);
-
-var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 8])
-    .on("zoom", move);
-
-var width = document.getElementById('container').offsetWidth-60;
-var height = width / 2;
-
-var topo,projection,path,svg,g;
-
-var tooltip = d3.select("#container").append("div").attr("class", "tooltip hidden");
-
-setup(width,height);
-
-function setup(width,height){
-  projection = d3.geo.mercator()
-    .translate([0, 0])
-    .scale(width / 2 / Math.PI);
-
-  path = d3.geo.path()
-      .projection(projection);
-
-  svg = d3.select("#container").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-      .call(zoom);
-
-  g = svg.append("g");
-
+function checkWidth(){
+  console.log("miew");
+  console.log(document.getElementById("container").offsetWidth);
+  var margin_left = document.getElementById("container").offsetWidth+20;
+  document.getElementById("checkbox_container").style.marginLeft = margin_left;
 }
 
-d3.json("world-topo.json", function(error, world) {
-
-  var countries = topojson.feature(world, world.objects.countries).features;
-
-  topo = countries;
-  draw(topo);
-
-});
-
-function draw(topo) {
-
-  var country = g.selectAll(".country").data(topo);
-
-  country.enter().insert("path")
-      .attr("class", "country")
-      .attr("d", path)
-      .attr("id", function(d,i) { return d.id; })
-      .attr("title", function(d,i) { return d.properties.name; })
-      .style("fill", function(d, i) { return d.properties.color; });
-
-  //ofsets plus width/height of transform, plus 20 px of padding, plus 20 extra for tooltip offset off mouse
-  var offsetL = document.getElementById('container').offsetLeft+(width/2)+40;
-  var offsetT =document.getElementById('container').offsetTop+(height/2)+20;
-
-  //tooltips
-  country
-    .on("mousemove", function(d,i) {
-      var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
-        tooltip
-          .classed("hidden", false)
-          .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
-          .html(d.properties.name)
-      })
-      .on("mouseout",  function(d,i) {
-        tooltip.classed("hidden", true)
-      }); 
-   
+function cocoa_check(){
+  console.log("cocoa_check")
 }
 
-function redraw() {
-  width = document.getElementById('container').offsetWidth-60;
-  height = width / 2;
-  d3.select('svg').remove();
-  setup(width,height);
-  draw(topo);
-}
+function choco_check(){
 
-function move() {
-
-  var t = d3.event.translate;
-  var s = d3.event.scale;  
-  var h = height / 3;
-  
-  t[0] = Math.min(width / 2 * (s - 1), Math.max(width / 2 * (1 - s), t[0]));
-  t[1] = Math.min(height / 2 * (s - 1) + h * s, Math.max(height / 2 * (1 - s) - h * s, t[1]));
-
-  zoom.translate(t);
-  g.style("stroke-width", 1 / s).attr("transform", "translate(" + t + ")scale(" + s + ")");
-
-}
-
-var throttleTimer;
-function throttle() {
-  window.clearTimeout(throttleTimer);
-    throttleTimer = window.setTimeout(function() {
-      redraw();
-    }, 200);
 }
