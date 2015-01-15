105b473e0b631dca3e9dac9a023b22a9ee0dcf98
Live session user <alixdodu@gmail.com>
2015-01-15 13:18:43 +0000

    Made file for matrix data

---
+++ Visualisation/data_files/matrixData_reader.py
@@ -0,0 +1,179 @@
+# Makes a json file that stores the important data of a csv 
+# file in a dictionnary.
+
+import csv
+import json
+
+csvFile = 'TradeMatrCocoaExpVal2011.csv'
+jsonFileName = '_TradeMatrCocoaExpVal2011.json'
+
+dataDic = {}
+
+with open(csvFile, 'rb') as csvfile:
+	csvReader = csv.reader(csvfile)
+
+	firstRow = next(csvReader)
+
+	lastCountry = ""
+
+	for row in csvReader:
+
+		# if row[5] == "European Union":
+		# 	continue
+		# elif row[5] == "South-Eastern Asia":
+		# 	continue
+		# elif row[5] == "Eastern Asia":
+		# 	continue
+		# elif row[5] == "Eastern Africa":
+		# 	continue
+		# elif row[5] == "Northern Africa":
+		# 	continue
+		# elif row[5] == "Western Africa":
+		# 	continue
+		# elif row[5] == "Eastern Africa":
+		# 	continue
+		# elif row[5] == "Southern Africa":
+		# 	continue
+		# elif row[5] == "Northern Europe":
+		# 	continue
+		# elif row[5] == "EU(25)ex.int":
+		# 	continue
+		# elif row[5] == "World":
+		# 	continue
+		# elif row[5] == "Asia":
+		# 	continue
+		# elif row[5] == "Europe":
+		# 	continue
+		# elif row[5] == "Africa":
+		# 	continue
+		# elif row[5] == "Oceania":
+		# 	continue
+		# elif row[5] == "Americas":
+		# 	continue
+		# elif row[5] == "Central Asia":
+		# 	continue
+		# elif row[5] == "Central America":
+		# 	continue
+		# elif row[5] == "Southern Asia":
+		# 	continue
+		# elif row[5] == "Western Asia":
+		# 	continue
+		# elif row[5] == "Middle Africa":
+		# 	continue
+		# elif row[5] == "China ex.int":
+		# 	continue
+		# elif row[5] == "Melanesia":
+		# 	continue
+		# elif row[5] == "EU(12)ex.int":
+		# 	continue
+		# elif row[5] == "EU(15)ex.int":
+		# 	continue
+		# elif row[5] == "EU(27)ex.int":
+		# 	continue
+		# elif row[5] == "Caribbean":
+		# 	continue
+		# elif row[5] == "Polynesia":
+		# 	continue
+		# elif row[5] == "Southern Europe":
+		# 	continue
+		# elif row[5] == "Eastern Europe":
+		# 	continue
+		# elif row[5] == "Western Europe":
+		# 	continue
+		# elif row[5] == "Northern Europe":
+		# 	continue
+		# elif row[5] == "Northern America":
+		# 	continue
+		# elif row[5] == "South America":
+		# 	continue
+		# elif row[5] == "Southern America":
+		# 	continue
+		# elif row[5] == "North America":
+		# 	continue
+		# elif row[5] == "Australia & New Zealand":
+		# 	continue
+		# elif row[5] == "Occupied Palestinian Territory":
+		# 	continue
+		# elif row[5] == "Small Island Developing States":
+		# 	continue
+		# elif row[5] == "Least Developed Countries":
+		# 	continue
+		# elif row[5] == "Low Income Food Deficit Countries":
+		# 	continue
+		# elif row[5] == "Land Locked Developing Countries":
+		# 	continue
+		# elif row[5] == "Net Food Importing Developing Countries":
+		# 	continue
+		if row[5] == "China, mainland":
+			continue
+		elif row[5] == "China, Hong Kong SAR":
+			continue
+		elif row[5] == "China, Macao SAR":
+			continue
+		elif row[5] == "China, Taiwan Province of":
+			continue
+
+		elif row[5] == "Bolivia (Plurinational State of)":
+			row[5] = "Bolivia, Plurinational State of"
+		elif row[5] == "Venezuela (Bolivarian Republic of)":
+			row[5] = "Venezuela, Bolivarian Republic of"
+		elif row[5] == "United States of America":
+			row[5] = "United States"
+		elif row[5] == "Republic of Moldova":
+			row[5] = "Moldova, Republic of"
+		elif row[5] == "The former Yugoslav Republic of Macedonia":
+			row[5] = "Macedonia, the former Yugoslav Republic of"
+		elif row[5] == "Iran (Islamic Republic of)":
+			row[5] = "Iran, Islamic Republic of"
+		elif row[5] == "United Republic of Tanzania":
+			row[5] = "Tanzania, United Republic of"
+		elif row[5] == "Democratic Republic of the Congo":
+			row[5] = "Congo, the Democratic Republic of the"
+		elif row[5] == "Republic of Korea":
+			row[5] = "Korea, Republic of"
+
+		if row[7] == "China, mainland":
+			continue
+		elif row[7] == "China, Hong Kong SAR":
+			continue
+		elif row[7] == "China, Macao SAR":
+			continue
+		elif row[7] == "China, Taiwan Province of":
+			continue
+
+		elif row[7] == "Bolivia (Plurinational State of)":
+			row[7] = "Bolivia, Plurinational State of"
+		elif row[7] == "Venezuela (Bolivarian Republic of)":
+			row[7] = "Venezuela, Bolivarian Republic of"
+		elif row[7] == "United States of America":
+			row[7] = "United States"
+		elif row[7] == "Republic of Moldova":
+			row[7] = "Moldova, Republic of"
+		elif row[7] == "The former Yugoslav Republic of Macedonia":
+			row[7] = "Macedonia, the former Yugoslav Republic of"
+		elif row[7] == "Iran (Islamic Republic of)":
+			row[7] = "Iran, Islamic Republic of"
+		elif row[7] == "United Republic of Tanzania":
+			row[7] = "Tanzania, United Republic of"
+		elif row[7] == "Democratic Republic of the Congo":
+			row[7] = "Congo, the Democratic Republic of the"
+		elif row[7] == "Republic of Korea":
+			row[7] = "Korea, Republic of"
+
+		# Makes a new dicitonnary entry when a new reporter 
+		# country is found.
+		if row[5] != lastCountry:
+			dataDic[row[5]] = {}
+
+		lastCountry = row[5]
+
+		dataDic[row[5]][row[7]] = int(row[15])
+		print dataDic
+		print row[5],row[7]
+
+with open(jsonFileName, "w") as f:
+	json.dump(dataDic, f, indent=4)
+
+
+
+	
--- Visualisation/script.js
+++ Visualisation/script.js
@@ -3,23 +3,30 @@
 
 
 function checkProp() {
-  var margin_left = document.getElementById("map_container").offsetWidth + 20;
-  var checkWidth = document.getElementById("header").offsetWidth - margin_left - 14;
-  document.getElementById("checkbox_container").style.marginLeft = margin_left.toString() + "px";
-  document.getElementById("checkbox_container").style.width = checkWidth.toString() + "px";
+	var margin_left = document.getElementById("map_container").offsetWidth + 20;
+	var checkWidth = document.getElementById("header").offsetWidth - margin_left - 14;	
+	document.getElementById("checkbox_container").style.marginLeft = margin_left.toString() + "px";
+	document.getElementById("checkbox_container").style.width = checkWidth.toString() + "px";
 }
 
 function cocoa_check() {
-  console.log("cocoa_check");
+	console.log("cocoa_check");
+    color_map("data_files/_CocoaExpVal2000_2011.json");
+	// color_map("data_files/_CocoaExpVal2000_2011.json", "quantile");
 }
 
 function choco_check() {
 
 }
 
-function color_map(dataFile) {
-// dataFile is a string of the filename with the data used to 
-// color the map.
+/*Colors the map according to the data stored in a JSON file.
+The data must be a dictionnary, with country names as keys and data
+values as values. One data value only per country.
+@ param dataFile: JSON file containing a dictionnary of the data.
+@ param scaleFunction: String that determines along which lines the color 
+scale is divided. Can be either "fraction" or "quantile".
+*/ 
+function color_map(dataFile, scale="fraction") {
 
 	d3.json(dataFile, color);
 
@@ -39,48 +46,65 @@ function color_map(dataFile) {
 		var dataMap = d3.map(data);
 		var dataValues1 = dataMap.values()
 		var dataValues = dataValues1.sort(compareNumbers)
-		console.log(dataValues)
+		// console.log(dataValues)
 
 		var max = d3.max(dataValues);
-		var oneFifth = max/5;
-		var twoFifth = 2*max/5;
-		var threeFifth = 3*max/5;
-		var fourFifth = 4*max/5;
 
-		console.log(oneFifth, twoFifth, threeFifth, fourFifth, max);
+		if (scale == "fraction") {
+			var oneFifth = max/5;
+			var twoFifth = 2*max/5;
+			var threeFifth = 3*max/5;
+			var fourFifth = 4*max/5;
 
-		var firstQuant = d3.quantile(dataValues, 0.2);
-		var secQuant = d3.quantile(dataValues, 0.4);
-		var thirdQuant = d3.quantile(dataValues, 0.6);
-		var fourthQuant = d3.quantile(dataValues, 0.8);
-		var fifthQuant = d3.quantile(dataValues, 1);
+			// console.log(oneFifth, twoFifth, threeFifth, fourFifth, max);
 
-		console.log(firstQuant, secQuant, thirdQuant, fourthQuant, fifthQuant);
-
-// # map.forEach(function)
-// Calls the specified function for each entry in this map, passing the entry's key and value as two arguments. The this context of the function is this map. Returns undefined. The iteration order is arbitrary.
-
-		for (var keyCountry in data) {
-			// console.log(keyCountry);
-			// console.log(data[keyCountry]);
-			document.getElementById(keyCountry).style.fill = "red";
-
-			if (data[keyCountry] < firstQuant) {
-				document.getElementById(keyCountry).style.fill = "blue";
-			}
-			if ((data[keyCountry] > firstQuant) && (data[keyCountry] < secQuant)) {
-				document.getElementById(keyCountry).style.fill = "green";
-			}
-			if ((data[keyCountry] > secQuant) && (data[keyCountry] < thirdQuant)) {
-				document.getElementById(keyCountry).style.fill = "yellow";
-			}
-			if ((data[keyCountry] > thirdQuant) && (data[keyCountry] < fourthQuant)) {
-				document.getElementById(keyCountry).style.fill = "orange";
+			for (var keyCountry in data) {
+				if (data[keyCountry] < oneFifth) {
+					document.getElementById(keyCountry).style.fill = "blue";
+				}
+				else if ((data[keyCountry] > oneFifth) && (data[keyCountry] < twoFifth)) {
+					document.getElementById(keyCountry).style.fill = "green";
+				}
+				else if ((data[keyCountry] > twoFifth) && (data[keyCountry] < threeFifth)) {
+					document.getElementById(keyCountry).style.fill = "yellow";
+				}
+				else if ((data[keyCountry] > threeFifth) && (data[keyCountry] < fourFifth)) {
+					document.getElementById(keyCountry).style.fill = "orange";
+				}
+				else if (data[keyCountry] > fourFifth) {
+					document.getElementById(keyCountry).style.fill = "red";
+				}
 			}
 		}
+		
+		if (scale == "quantile") {
+			var firstQuant = d3.quantile(dataValues, 0.2);
+			var secQuant = d3.quantile(dataValues, 0.4);
+			var thirdQuant = d3.quantile(dataValues, 0.6);
+			var fourthQuant = d3.quantile(dataValues, 0.8);
+			var fifthQuant = d3.quantile(dataValues, 1);
+
+			// console.log(firstQuant, secQuant, thirdQuant, fourthQuant, fifthQuant);
+
+			for (var keyCountry in data) {
+				if (data[keyCountry] < firstQuant) {
+					document.getElementById(keyCountry).style.fill = "blue";
+				}
+				else if ((data[keyCountry] > firstQuant) && (data[keyCountry] < secQuant)) {
+					document.getElementById(keyCountry).style.fill = "green";
+				}
+				else if ((data[keyCountry] > secQuant) && (data[keyCountry] < thirdQuant)) {
+					document.getElementById(keyCountry).style.fill = "yellow";
+				}
+				else if ((data[keyCountry] > thirdQuant) && (data[keyCountry] < fourthQuant)) {
+					document.getElementById(keyCountry).style.fill = "orange";
+				}
+				else if (data[keyCountry] > fourthQuant) {
+					document.getElementById(keyCountry).style.fill = "red";
+				}
+			}	
+		}
 	}
-//d3.map om lijst te maken van dic
 }
 
-// color_map("data_files/_CocoaExpVal2000_2011.json");
-color_map("data_files/_CocoaExpVal2000_2011.json");
+
